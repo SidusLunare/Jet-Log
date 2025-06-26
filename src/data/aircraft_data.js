55fd68a3af85f25d95d1ed82bcb95afe49abd45b
SidusLunare <stefanpftf@gmail.com>
2025-06-26 14:40:50 +0200

    v0.0
    start for exam

---
+++ src/data/aircraft_data.js
@@ -0,0 +1,101 @@
+// Aircraft Types and Airlines Data for Spotting Logbook
+
+export const aircraftTypes = [
+  // Boeing
+  "Boeing 737-800",
+  "Boeing 737-900",
+  "Boeing 737 MAX 8",
+  "Boeing 747-400",
+  "Boeing 747-8F",
+  "Boeing 757-200",
+  "Boeing 767-300",
+  "Boeing 777-200",
+  "Boeing 777-300ER",
+  "Boeing 787-8 Dreamliner",
+  "Boeing 787-9 Dreamliner",
+  
+  // Airbus
+  "Airbus A319",
+  "Airbus A320",
+  "Airbus A321",
+  "Airbus A330-200",
+  "Airbus A330-300",
+  "Airbus A340-300",
+  "Airbus A350-900",
+  "Airbus A380",
+  
+  // Embraer
+  "Embraer E190",
+  "Embraer E195",
+  "Embraer ERJ-145",
+  
+  // Bombardier
+  "Bombardier CRJ-700",
+  "Bombardier CRJ-900",
+  "Bombardier Q400",
+  
+  // Other
+  "ATR 72",
+  "Fokker 70",
+  "Fokker 100",
+  "McDonnell Douglas MD-11",
+  "Antonov An-124",
+  "Lockheed C-130 Hercules"
+];
+
+export const airlines = [
+  // Dutch
+  "KLM Royal Dutch Airlines",
+  "KLM Cityhopper", 
+  "Transavia",
+  "Corendon Airlines",
+  
+  // European
+  "Lufthansa",
+  "Air France",
+  "British Airways",
+  "Ryanair",
+  "EasyJet",
+  "Vueling",
+  "SAS Scandinavian Airlines",
+  "Swiss International Air Lines",
+  "Austrian Airlines",
+  "TAP Air Portugal",
+  "Iberia",
+  "Alitalia",
+  "Turkish Airlines",
+  
+  // American
+  "Delta Air Lines",
+  "American Airlines",
+  "United Airlines",
+  "Southwest Airlines",
+  
+  // Middle Eastern
+  "Emirates",
+  "Qatar Airways",
+  "Etihad Airways",
+  
+  // Asian
+  "Singapore Airlines",
+  "Cathay Pacific",
+  "Japan Airlines",
+  "ANA All Nippon Airways",
+  "China Southern Airlines",
+  
+  // Cargo
+  "DHL Air",
+  "FedEx Express",
+  "UPS Airlines",
+  "Cargolux",
+  
+  // Low Cost
+  "Wizz Air",
+  "Norwegian Air",
+  "Jet2.com"
+];
+
+export default {
+  aircraftTypes,
+  airlines
+};
